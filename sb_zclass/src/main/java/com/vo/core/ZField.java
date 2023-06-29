d4874082c076d64be5c77df042cc345129937ca6
zhangzhen <1071324756@qq.com>
2023-06-29 20:36:02 +0800

    ZField 加入注解值

--- sb_zclass/src/main/java/com/vo/core/ZField.java
+++ sb_zclass/src/main/java/com/vo/core/ZField.java
@@ -1,5 +1,8 @@
 package com.vo.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,10 +25,43 @@ public class ZField {
 
 	private Object value;
 
+	private List<String> annoList;
+
+
+	public void addAnno(final String annoString) {
+		this.initAnnoList();
+		this.annoList.add(annoString);
+	}
+
+	public void addAnno(final Class annoClass) {
+		this.annoList.add("@" + annoClass.getCanonicalName());
+	}
+
+	private void initAnnoList() {
+		if(this.annoList == null) {
+			this.annoList = new ArrayList<>();
+		}
+	}
+
 	@Override
 	public String toString() {
-		final String s = this.getType() + " " + this.getName() +  " = " + this.getValue() + ";";
-		return s;
+		final StringBuilder builder = new StringBuilder();
+		if (this.annoList != null) {
+			for (final String as : this.annoList) {
+				builder.append(as).append("\r\n");
+			}
+		}
+
+		final StringBuilder append = builder.append(this.getType()).append( " ") .append(this.getName()).append(" = ").append(this.getValue()).append(";");
+
+//		final String s = this.getType() + " " + this.getName() + " = " + this.getValue() + ";";
+		return append.toString();
+	}
+
+	public ZField(final String type, final String name, final Object value) {
+		this.type = type;
+		this.name = name;
+		this.value = value;
 	}
 
 }
