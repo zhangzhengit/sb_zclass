8f3005494232f1ec6e3175cc2896e2f8539a3580
zhangzhen <1071324756@qq.com>
2025-09-01 01:31:46 +0800

    去除spring相关依赖

--- sb_zclass/src/main/java/com/vo/SbZClassApplication.java
+++ sb_zclass/src/main/java/com/vo/SbZClassApplication.java
@@ -1,9 +1,5 @@
 package com.vo;
 
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 /**
  * 运行时动态构造字符串形式的java源码，并且动态编译出java对象
  *
@@ -11,11 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2023年6月11日
  *
  */
-//@SpringBootApplication
 public class SbZClassApplication {
 
 	public static void main(final String[] args) {
-		SpringApplication.run(SbZClassApplication.class, args);
 	}
 
 }
